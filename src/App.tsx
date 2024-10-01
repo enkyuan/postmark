efd854fee0cd33ecf9bdf641692546679b674177
enkyuan <yuan_enkang@outlook.com>
2024-10-01 10:57:48 -0500

    linted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 // FIXME: frappe-gantt package may not be working
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Auth } from './components/Header'
 import Router from './Router';
 import './App.css'
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from "react-router-dom"
 import { ArrowUpRight } from "@phosphor-icons/react"
 import postmark from "../assets/wordmark.svg"
 
